76719e2e71bd9086d9919817e83591ff79b755ce
RidhaMuneer <ridhamuner@gmail.com>
2024-08-14 23:07:25 +0300

    fix: hover state for icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,3 @@
-// nextjs
-import Image from "next/image";
-
 // data
 import { footerIconLinks } from "@/data/icons";
 
@@ -9,19 +6,19 @@ import styles from "@/app/page.module.css";
 
 /**
  * Footer component that displays a row of social media icons.
- * 
- * This component maps over an array of icon paths and renders each icon using the `Image` component from Next.js.
- * 
+ *
+ * This component maps over an array of icon paths and renders each icon using a div.
+ *
  * @component
- * 
+ *
  * @example
  * ```jsx
  * import Footer from '@/components/Footer';
- * 
+ *
  * function App() {
  *   return <Footer />;
  * }
- * 
+ *
  * export default App;
  * ```
  */
@@ -30,13 +27,10 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       {footerIconLinks.map((icon, index) => (
-        <Image
+        <div
           key={index}
-          src={icon}
-          alt={icon}
-          width={30}
-          height={30}
           className={styles.icon}
+          style={{ backgroundImage: `url(${icon})` }}
         />
       ))}
     </footer>
